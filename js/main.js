d7bb54feca3716d1df2003e496749da89d581413
Jack Kufa <jack@kufa.io>
2020-07-26 03:22:16 -0500

    Work on Expereince section

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+  let $btns = $('.experience .button-group button');
+  $btns.click(function(e){
+    $('.experience .button-group button').removeClass('active');
+    e.target.classList.add('active');
+
+    let selector = $(e.target).attr('data-filter');
+    $('.experience .grid').isotope({
+      filter: selector
+    });
+    return false;
+  })
+  // $('.experience .button-group #btn1').trigger('click');
+
+  // $('.experience .grid .test-popup-link').magnificPopup({
+  //     type: 'image',
+  //     gallery: { enabled: true }
+  // });
+});
