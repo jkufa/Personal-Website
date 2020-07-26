b3ac6d1a9d93a1adc4069b9d879d96e2f4a0220c
Jack Kufa <jack@kufa.io>
2020-07-26 15:28:52 -0500

    Add pagePiling js

--- js/main.js
+++ js/main.js
@@ -17,7 +17,7 @@ $(document).ready(function() {
       type: 'image',
       gallery: { enabled: true }
   });
-
+// Projects
   let $btn_proj = $('.projects .button-group button');
   $btn_proj.click(function(e){
     $('.projects .button-group button').removeClass('active');
@@ -35,4 +35,35 @@ $(document).ready(function() {
       type: 'image',
       gallery: { enabled: true }
   });
+
+  // Pagepiling
+  $('#pagepiling').pagepiling({
+    menu: null,
+      direction: 'vertical',
+      verticalCentered: true,
+      sectionsColor: [],
+      anchors: [],
+      scrollingSpeed: 700,
+      easing: 'swing',
+      loopBottom: false,
+      loopTop: false,
+      css3: true,
+      navigation: {
+          'textColor': '#000',
+          'bulletsColor': '#000',
+          'position': 'right',
+          'tooltips': ['section1', 'section2', 'section3', 'section4']
+      },
+       normalScrollElements: null,
+      normalScrollElementTouchThreshold: 5,
+      touchSensitivity: 5,
+      keyboardScrolling: true,
+      sectionSelector: '.section',
+      animateAnchor: false,
+
+  //events
+  onLeave: function(index, nextIndex, direction){},
+  afterLoad: function(anchorLink, index){},
+  afterRender: function(){},
+});
 });
