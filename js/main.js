169910464f0cb8f56941621c7d4c027cf64a6ecb
Jack Kufa <jack@kufa.io>
2020-07-26 17:17:27 -0500

    work on integrating pagePiling

--- js/main.js
+++ js/main.js
@@ -38,32 +38,13 @@ $(document).ready(function() {
 
   // Pagepiling
   $('#pagepiling').pagepiling({
-    menu: null,
-      direction: 'vertical',
-      verticalCentered: true,
-      sectionsColor: [],
-      anchors: [],
-      scrollingSpeed: 700,
-      easing: 'swing',
-      loopBottom: false,
-      loopTop: false,
-      css3: true,
-      navigation: {
-          'textColor': '#000',
-          'bulletsColor': '#000',
+    menu: '#menu',
+    anchors: ['page1', 'page2', 'page3'],
+    navigation: {
+          'textColor': '#f2f2f2',
+          'bulletsColor': '#ccc',
           'position': 'right',
-          'tooltips': ['section1', 'section2', 'section3', 'section4']
-      },
-       normalScrollElements: null,
-      normalScrollElementTouchThreshold: 5,
-      touchSensitivity: 5,
-      keyboardScrolling: true,
-      sectionSelector: '.section',
-      animateAnchor: false,
-
-  //events
-  onLeave: function(index, nextIndex, direction){},
-  afterLoad: function(anchorLink, index){},
-  afterRender: function(){},
+          'tooltips': ['Page 1', 'Page 2', 'Page 3', 'Page 4']
+      }
 });
 });
