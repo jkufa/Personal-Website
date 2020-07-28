3caac3e9c831a4d7b2217225ca305f92d95d891d
Jack Kufa <jack@kufa.io>
2020-07-28 02:30:23 -0500

    Fix projects to add inline

--- js/main.js
+++ js/main.js
@@ -11,17 +11,33 @@ $(document).ready(function() {
     });
     return false;
   })
-  $('.experience .button-group #btn1').trigger('click');
+  // Projects
+  let $btn_proj = $('.projects .button-group button');
+  $btn_proj.click(function(e){
+    $('.projects .button-group button').removeClass('active');
+    e.target.classList.add('active');
 
+    let selector = $(e.target).attr('data-filter');
+    $('.projects .grid').isotope({
+      filter: selector
+    });
+    return false;
+  })
+  
   $('.experience .grid .popup-link').magnificPopup({
-      type: 'inline',
-      midClick: true, // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
-      gallery: { enabled: true }
-  });
+    type: 'inline',
+    midClick: true, // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
+    gallery: { enabled: true }
+});
+
+$('.projects .grid .popup-link2').magnificPopup({
+    type: 'inline',
+    midClick: true, // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
+    gallery: { enabled: true }
+});
 
   // Pagepiling
   $('#pagepiling').pagepiling({
-    menu: null,
     direction: 'vertical',
     anchors: ['home','about','experience','projects','contact'],    
     navigation: {
