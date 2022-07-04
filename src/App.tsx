dbd79dea3d2f7206e08523aa6ecbfaa063388b5d
Jack Kufa <jack@kufa.io>
2022-07-04 13:06:27 -0400

    css for sections

--- src/App.tsx
+++ src/App.tsx
@@ -14,22 +14,39 @@ const App = () => {
   return (
     <div className='App'>
       <div className='loading-frame'><div className='container'></div></div>
-      <div className='container'>
-        <Particles options={particlesOptions as ISourceOptions} init={particlesInit}/>
-        <div className='flex-item'>
-          <div className='content'>
-            <h1>Jack Kufa is pretty much guy.</h1>
-            <div><nav><ul>
-              <li><button autoFocus>Home</button></li>
-              <li><button>About</button></li>
-              <li><button>Experience</button></li>
-              <li><button>Resume</button></li>
-            </ul></nav></div>
+      <section id='home'>
+        <div className='container'>
+          <Particles options={particlesOptions as ISourceOptions} init={particlesInit}/>
+          <div className='flex-item'>
+            <div className='content'>
+              <h1>Jack Kufa is pretty much guy.</h1>
+              <div><nav><ul>
+                <li><button autoFocus>Home</button></li>
+                <li><button>About</button></li>
+                <li><button>Experience</button></li>
+                <li><button onClick={() => window.location.href=''}>Resume</button></li>
+              </ul></nav></div>
+            </div>
           </div>
         </div>
-    </div>
+      </section>
+      {/* <section id='about'>
+        <h1>About</h1>
+      </section>
+      <section id='experience'>
+        <h1>Experience</h1>
+      </section> */}
   </div>
   );
 }
 
 export default App;
+
+// guy
+// front-end
+// coffee
+// design
+// accessibility
+// music
+// learning guitar
+// mustache
