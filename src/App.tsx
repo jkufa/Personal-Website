26d13a205837f88c91b7f5f4cbf9e4d3a4bfe263
Jack Kufa <jack@kufa.io>
2022-07-03 22:50:46 -0400

    Init react app

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.scss';
+
+const App = () => {
+  return (
+    <div className="container">
+    <div className="flex-item">
+      <div className="content">
+        <h1>Jack Kufa is pretty much guy.</h1>
+        <div><nav><ul>
+          <li><button autoFocus>Home</button></li>
+          <li><button>About</button></li>
+          <li><button>Experience</button></li>
+          <li><button>Resume</button></li>
+        </ul></nav></div>
+      </div>
+    </div>
+  </div>
+  );
+}
+
+export default App;
