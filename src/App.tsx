0794d7419958aeb38945eb89a00df168e3cb6b00
Jack Kufa <jack@kufa.io>
2022-07-04 02:21:02 -0400

    Add transition animation & more styling

--- src/App.tsx
+++ src/App.tsx
@@ -12,19 +12,21 @@ const App = () => {
     await loadFull(engine);
 }, []);
   return (
-    <div className='container'>
-    <div className='loadingFrame'></div>
-    <Particles options={particlesOptions as ISourceOptions} init={particlesInit}/>
-    <div className='flex-item'>
-      <div className='content'>
-        <h1>Jack Kufa is pretty much guy.</h1>
-        <div><nav><ul>
-          <li><button autoFocus>Home</button></li>
-          <li><button>About</button></li>
-          <li><button>Experience</button></li>
-          <li><button>Resume</button></li>
-        </ul></nav></div>
-      </div>
+    <div className='App'>
+      <div className='loading-frame'><div className='container'></div></div>
+      <div className='container'>
+        <Particles options={particlesOptions as ISourceOptions} init={particlesInit}/>
+        <div className='flex-item'>
+          <div className='content'>
+            <h1>Jack Kufa is pretty much guy.</h1>
+            <div><nav><ul>
+              <li><button autoFocus>Home</button></li>
+              <li><button>About</button></li>
+              <li><button>Experience</button></li>
+              <li><button>Resume</button></li>
+            </ul></nav></div>
+          </div>
+        </div>
     </div>
   </div>
   );
