abeec6cf9678e61ff7f06d45ffbd6d8846c18007
Jack Kufa <jack@kufa.io>
2023-11-10 20:34:31 -0500

    Clean slate (sveltekit)

---
+++ src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import Lenis from '@studio-freight/lenis';
+	import { onMount } from 'svelte';
+
+  let lenis: Lenis;
+
+  onMount(() => {
+    lenis = new Lenis();
+  
+    lenis.on('scroll', (e: number) => {
+      console.log(e);
+    })
+  
+    function raf(time: number) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+  
+    requestAnimationFrame(raf);
+  });
+</script>
